eae66ce9fcbf813c29f4051ef767f4366d0e4980
SimSBgit <sbct47@gmail.com>
2025-10-27 12:06:57 +0900

    수정
    10/27
    12:06

---
+++ src/main/java/com/example/ex25_branch/controller/StudentRestController.java
@@ -0,0 +1,65 @@
+package com.example.ex25_branch.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ex25_branch.domain.Student;
+import com.example.ex25_branch.service.StudentService;
+
+import lombok.RequiredArgsConstructor;
+
+
+
+
+@RestController
+@RequestMapping("/api/students")
+@RequiredArgsConstructor
+public class StudentRestController {
+
+	private final StudentService studentSerivce;
+	
+//	전체 조회
+	@GetMapping
+	public List<Student> list() {
+		return studentSerivce.getAllStudent();
+	}
+	
+//	개별 정보 가져오기
+	@GetMapping("/{id}")
+	public ResponseEntity<Student> detail(@PathVariable Long id) {
+		Student student = studentSerivce.getStudent(id);
+		return ResponseEntity.ok(student);
+	}
+	
+//	등록
+	@PostMapping
+	public ResponseEntity<Student> create(@RequestBody Student student) {
+		studentSerivce.createStudent(student);
+		return ResponseEntity.ok(student);
+	}
+	
+//	개별 정보 수정 처리
+	@PutMapping("/{id}")
+	public ResponseEntity<Student> update(@PathVariable Long id, @RequestBody Student student) {
+
+		student.setId(id);
+		studentSerivce.updateStudent(student);
+		return ResponseEntity.ok(student);
+	}
+	
+//	삭제
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		studentSerivce.deleteStudent(id);
+		return ResponseEntity.ok().build();
+	} 
+}
