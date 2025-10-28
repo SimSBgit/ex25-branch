d79b717091e968122f28d9909db9a8cb4d07369f
SungBoSim <sbct47@gmail.com>
2025-10-28 17:19:11 +0900

    Revert "Merge branch 'f2/mappersql' into dev3/all"
    This reverts commit bc5216ee912e8c96d5ff1b303f0fbbb115a827fb, reversing
    changes made to 7bd4c1179d3afa7c0a6e5d005a2afe8f11245724.

--- src/main/java/com/example/ex25_branch/Ex25BranchApplication.java
+++ src/main/java/com/example/ex25_branch/Ex25BranchApplication.java
@@ -1,10 +1,8 @@
 package com.example.ex25_branch;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@MapperScan("com.example.ex25_branch.mapper")
 @SpringBootApplication
 public class Ex25BranchApplication {
 
--- src/main/java/com/example/ex25_branch/mapper/StudentMapper.java
+++ src/main/java/com/example/ex25_branch/mapper/StudentMapper.java
@@ -2,12 +2,7 @@ package com.example.ex25_branch.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 import com.example.ex25_branch.domain.Student;
 
@@ -16,24 +11,12 @@ public interface StudentMapper {
 
 	List<Student> findAll();
 
-//	Student findById(Long id);
-	@Select("select * from student where id = #{id}")
 	Student findById(Long id);
-	
 
-//	void insert(Student student);
-	@Insert("INSERT INTO student(NAME, email, age) \r\n"
-			+ "VALUES(#{name}, #{email}, #{age})")
-	@Options(useGeneratedKeys = true, keyProperty = "id")
 	void insert(Student student);
 
-//	void update(Student student);
-	@Update("UPDATE student SET NAME = #{name}, email = #{email}, age = #{age}\r\n"
-			+ "WHERE id = #{id}")
 	void update(Student student);
 
-//	void delete(Long id);
-	@Delete("DELETE FROM student WHERE id = #{id}")
 	void delete(Long id);
 
 
